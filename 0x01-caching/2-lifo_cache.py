180c9b97f47986dcd73439242cf55c90c5092738
Aymen <aymenabduseid10@gmail.com>
2023-07-25 15:43:44 -0400

    hot fix

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -4,13 +4,15 @@ from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """LIFOCache"""
+    """
+    class LIFOCache
+    """
     def __init__(self):
         super().__init__()
 
     def put(self, key, item):
-        """
-        put
+        """assign to the dictionary self.cache_data the item value for the
+        key key.
         """
         if key is None or item is None:
             return
@@ -23,8 +25,7 @@ class LIFOCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
-        """
-        get
+        """Return the value in self.cache_data linked to key.
         """
 
         return self.cache_data.get(key, None)
--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -4,13 +4,17 @@ from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """LRUCache"""
+    """
+    class LRUCache
+    """
     def __init__(self):
         super().__init__()
         self.used_key = []
 
     def put(self, key, item):
-        """put"""
+        """assign to the dictionary self.cache_data the item value
+            for the key key.
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
             if key not in self.used_key:
@@ -24,7 +28,8 @@ class LRUCache(BaseCaching):
                 print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
-        """get"""
+        """Return the value in self.cache_data linked to key.
+        """
         if key is not None and key in self.cache_data.keys():
             self.used_key.append(self.used_key.pop(self.used_key.index(key)))
         return self.cache_data.get(key, None)
