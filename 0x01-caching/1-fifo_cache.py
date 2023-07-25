a906271240572367549b7c652a06d7278aaf50e6
Aymen <aymenabduseid10@gmail.com>
2023-07-25 15:38:18 -0400

    hot fix

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-"""
-FIFO caching
-"""
+""" FIFO caching """
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """FIFOCache"""
+    """class FIFOCache"""
     def __init__(self):
         super().__init__()
 
     def put(self, key, item):
-        """put"""
+        """assign to the dictionary self.cache_data the item value
+            for the key key
+        """
         if key is None or item is None:
             return
 
@@ -23,6 +23,7 @@ class FIFOCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
-        """get"""
+        """Return the value in self.cache_data linked to key.
+        """
 
         return self.cache_data.get(key, None)
